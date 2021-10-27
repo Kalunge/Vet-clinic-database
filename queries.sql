bcb170b1b9597c0038a85d2419eb0717534406de
kalunge <muthomititus+github@gmail.com>
2021-10-27 18:16:07 +0100

    Add queries to answer analytical questions

--- queries.sql
+++ queries.sql
@@ -14,3 +14,14 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals  WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+
+
+
+SELECT name, full_name FROM animals INNER JOIN owners ON animals.owners_id = owners.id WHERE owners.id = 4;
+SELECT a.name as animal, s.name as type FROM animals a INNER  JOIN species s ON a.species_id = s.id WHERE  s.name = 'Pokemon';
+SELECT full_name as owner, name as animal FROM owners LEFT JOIN animals ON owners.id = animals.owners_id; 
+SELECT COUNT(a), s.name FROM animals a JOIN species s ON a.species_id = s.id GROUP BY s.name;
+SELECT name FROM animals JOIN owners on animals.owners_id = owners.id WHERE full_name = 'Jennifer Orwell' AND species_id=2;
+SELECT name FROM animals JOIN owners ON animals.owners_id = owners.id WHERE escape_attempts < 1 AND full_name = 'Dean Winchester';
+SELECT full_name, COUNT(name) AS num_of_animals FROM animals JOIN owners ON owners.id = animals.owners_id GROUP BY full_name ORDER BY num_of_animals DESC LIMIT 1;
